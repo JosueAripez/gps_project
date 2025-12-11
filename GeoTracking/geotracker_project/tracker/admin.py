116f34ae98d03c565fad38cafdec24060e683145
JosueAripez <124739505+JosueAripez@users.noreply.github.com>
2025-12-11 11:39:26 -0800

    Delete GeoTracking directory

--- GeoTracking/geotracker_project/tracker/admin.py
+++
@@ -1,8 +0,0 @@
-# tracker/admin.py
-from django.contrib import admin
-from .models import Device, Geofence, Location, Alert
-
-admin.site.register(Device)
-admin.site.register(Geofence)
-admin.site.register(Location)
-admin.site.register(Alert)
